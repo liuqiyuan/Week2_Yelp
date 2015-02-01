d361e58d1a5268346cd759c9bf5c223fb6d35595
liuqiyuan <i@liuqiyuan.com>
2015-01-31 21:24:00 -0800

    empty project checkin

---
+++ Qi's Yelp/Qi's Yelp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Qi's Yelp
+//
+//  Created by Qiyuan Liu on 1/31/15.
+//  Copyright (c) 2015 liuqiyuan.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
